f68d2a1b932c2995de3930af8dbf6a73dc2b275c
MiniDigger <admin@minidigger.me>
2020-09-30 17:51:01 +0200

    fix entrypoint to use the right creds

--- entrypoint/run.sh
+++ entrypoint/run.sh
@@ -7,8 +7,9 @@ cd /app
 # wait for database to be ready
 touch ~/.pgpass
 chmod 0600 ~/.pgpass
-echo "db:5432:hangarauth:hangarauth:hangarauth" > ~/.pgpass
-until psql -w -h 'db' -U hangarauth hangarauth -c '\l'; do
+# todo use creds from env...
+echo "hangar_db:5432:hangar_auth:hangar:hangar" > ~/.pgpass
+until psql -w -h 'hangar_db' -U hangar hangar -c '\l'; do
   echo "Postgres isn't ready yet..." >&2
   sleep 1
 done
