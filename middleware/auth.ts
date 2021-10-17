7ffe70625eafc0473893540dcaa26d05448db81e
MiniDigger <admin@benndorf.dev>
2021-10-17 20:37:37 +0200

    debug auth middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -3,17 +3,21 @@ import { addMiddleware } from '~/utils/middleware';
 
 export function AuthRequired() {
     const middleware: Middleware = ({ $kratos, store }: Context) => {
+        console.debug('fetching session...');
         return $kratos.client
             .toSession(undefined, undefined, { withCredentials: true })
             .then((session) => {
+                console.debug('got result', session);
                 if (session.data && session.data.active) {
                     store.commit('SET_USER', session.data.identity);
                     return;
                 }
+                console.debug('no session -> login');
                 return $kratos.login();
             })
             .catch((e) => {
                 if (e.response.status === 401) {
+                    console.debug('401 -> login');
                     return $kratos.login();
                 }
                 console.log(e);
