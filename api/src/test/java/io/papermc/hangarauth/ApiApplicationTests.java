2efca93272e185c3df3d1c4ef679cae875cca373
Jake Potrebic <jake.m.potrebic@gmail.com>
2021-10-22 18:05:26 -0700

    add fake database for tests

--- api/src/test/java/io/papermc/hangarauth/ApiApplicationTests.java
+++ api/src/test/java/io/papermc/hangarauth/ApiApplicationTests.java
@@ -2,6 +2,13 @@ package io.papermc.hangarauth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
 
 @SpringBootTest
 class ApiApplicationTests {
@@ -10,4 +17,14 @@ class ApiApplicationTests {
     void contextLoads() {
     }
 
+    @Bean
+    @Primary
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+            .generateUniqueName(true)
+            .setType(EmbeddedDatabaseType.H2)
+            .setScriptEncoding("UTF-8")
+            .ignoreFailedDrops(true)
+            .build();
+    }
 }
