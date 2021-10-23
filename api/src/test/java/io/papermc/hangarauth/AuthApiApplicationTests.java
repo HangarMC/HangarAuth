34a9fdfc7aacdc1b509a366c4534cfa7fb947efd
Jake Potrebic <jake.m.potrebic@gmail.com>
2021-10-22 19:36:42 -0700

    use test containers for 'mock' postgres database

---
+++ api/src/test/java/io/papermc/hangarauth/AuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.papermc.hangarauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ api/src/test/resources/test_migrations/V0.0.0__initTests.sql
@@ -0,0 +1,5 @@
+-- dummy tables for tests (meant to simulate tables created by kratos)
+CREATE TABLE IF NOT EXISTS identities
+(
+    id uuid NOT NULL CONSTRAINT identities_pkey PRIMARY KEY
+);
