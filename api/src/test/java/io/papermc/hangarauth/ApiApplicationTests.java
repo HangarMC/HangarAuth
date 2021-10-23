b3c19fdfce3222675fe86cd9cf3351d125758bd2
Jake Potrebic <jake.m.potrebic@gmail.com>
2021-10-22 18:13:31 -0700

    fix tests

--- api/src/test/java/io/papermc/hangarauth/ApiApplicationTests.java
+++ api/src/test/java/io/papermc/hangarauth/ApiApplicationTests.java
@@ -1,30 +1,22 @@
 package io.papermc.hangarauth;
 
+import io.papermc.hangarauth.config.TestDatabaseConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
 
-@SpringBootTest
+@SpringBootTest(classes = TestDatabaseConfig.class)
 class ApiApplicationTests {
 
     @Test
     void contextLoads() {
     }
 
-    @Bean
-    @Primary
-    public DataSource dataSource() {
-        return new EmbeddedDatabaseBuilder()
-            .generateUniqueName(true)
-            .setType(EmbeddedDatabaseType.H2)
-            .setScriptEncoding("UTF-8")
-            .ignoreFailedDrops(true)
-            .build();
-    }
 }
