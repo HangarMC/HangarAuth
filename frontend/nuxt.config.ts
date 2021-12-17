bfba8ec005366cfd779cbc1b6b510dd0d5620bfb
MiniDigger <admin@benndorf.dev>
2021-12-17 19:39:11 +0100

    guess this was important

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -92,6 +92,7 @@ export default {
 
     // Build Configuration: https://go.nuxtjs.dev/config-build
     build: {
+        transpile: ['lodash-es'],
         parallel: true,
         cache: true,
         babel: {
