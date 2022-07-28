d13eb04b7d631ee68dcd6e0c9c388a4472a78793
MiniDigger | Martin <admin@benndorf.dev>
2022-07-28 23:21:51 +0200

    add identify filter

--- backend/src/main/java/io/papermc/hangarauth/config/WebConfig.java
+++ backend/src/main/java/io/papermc/hangarauth/config/WebConfig.java
@@ -6,10 +6,15 @@ import org.springframework.http.MediaType;
 import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @EnableWebMvc
 @Configuration
@@ -32,6 +37,16 @@ public class WebConfig implements WebMvcConfigurer {
         final ByteArrayHttpMessageConverter converter = new ByteArrayHttpMessageConverter();
         converter.setSupportedMediaTypes(this.getSupportedMediaTypes());
         return converter;
+    }
+
+    @Bean
+    public Filter identifyFilter() {
+        return new OncePerRequestFilter() {
+            @Override
+            protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
+                response.setHeader("Server", "HangarAuth");
+            }
+        };
     }
 
     private List<MediaType> getSupportedMediaTypes() {
