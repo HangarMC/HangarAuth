7305b6ff21f8484d42ae01a1adc7c5b2fb3710ea
MiniDigger | Martin <admin@benndorf.dev>
2022-10-31 10:56:20 +0100

    feat: implement notifications

--- frontend/src/layouts/default.vue
+++ frontend/src/layouts/default.vue
@@ -4,6 +4,7 @@
     <Container class="mb-24 md:mb-16">
       <slot />
     </Container>
+    <Notifications />
     <Footer />
   </div>
 </template>
@@ -12,6 +13,7 @@
 import Header from "~/components/Header.vue";
 import Footer from "~/components/Footer.vue";
 import Container from "~/lib/components/design/Container.vue";
+import Notifications from "~/lib/components/design/Notifications.vue";
 
 definePageMeta({
   title: "HangarAuth",
