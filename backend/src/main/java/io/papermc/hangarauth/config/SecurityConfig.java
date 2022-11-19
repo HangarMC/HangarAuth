2b2fe9131e16f8027cdbdb67a9c813ea105233ea
MiniDigger | Martin <admin@benndorf.dev>
2022-11-19 15:16:26 +0100

    fix: hopefully stop undertow from appending jsessionid by disabling session

---
+++ backend/src/main/java/io/papermc/hangarauth/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package io.papermc.hangarauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); // no session pls
+        http.csrf().disable(); // no csrf, we are stateless
+        return http.build();
+    }
+}
