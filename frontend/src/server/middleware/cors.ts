f135f4067602a61bd02259acf4d66bedf4bd5dae
MiniDigger | Martin <admin@benndorf.dev>
2022-12-22 16:17:35 +0100

    feat(frontend): cache-control headers + fixup cors middleware

--- frontend/src/server/middleware/cors.ts
+++ frontend/src/server/middleware/cors.ts
@@ -1,9 +1,9 @@
 import { defineEventHandler } from "h3";
 
 export default defineEventHandler((event) => {
-  const origin = event.req.headers.origin;
-  if (origin && ["http://localhost:3333", "https://hangar.benndorf.dev"].includes(origin)) {
-    event.res.setHeader("Access-Control-Allow-Origin", origin);
-    event.res.setHeader("Access-Control-Allow-Credentials", "true");
+  const origin = event.node.req.headers.origin;
+  if (origin && ["http://localhost:3333", "https://hangar.benndorf.dev", "https://hangar.papermc.io", "https://hangar.papermc.dev"].includes(origin)) {
+    event.node.res.setHeader("Access-Control-Allow-Origin", origin);
+    event.node.res.setHeader("Access-Control-Allow-Credentials", "true");
   }
 });
