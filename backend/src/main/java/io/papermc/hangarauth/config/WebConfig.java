5a33be1376aa199f97ca5c72d80c1e16c9f3ad55
Jake Potrebic <jake.m.potrebic@gmail.com>
2022-12-24 13:08:36 -0800

    fix: settings icon cors issue

--- backend/src/main/java/io/papermc/hangarauth/config/WebConfig.java
+++ backend/src/main/java/io/papermc/hangarauth/config/WebConfig.java
@@ -1,5 +1,13 @@
 package io.papermc.hangarauth.config;
 
+import io.papermc.hangarauth.config.custom.GeneralConfig;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,16 +20,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.io.IOException;
-import java.util.List;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import io.papermc.hangarauth.config.custom.GeneralConfig;
-
 @EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -29,20 +27,20 @@ public class WebConfig implements WebMvcConfigurer {
     private final GeneralConfig generalConfig;
 
     @Autowired
-    public WebConfig(GeneralConfig generalConfig) {
+    public WebConfig(final GeneralConfig generalConfig) {
         this.generalConfig = generalConfig;
     }
 
     @Bean
-    public RestTemplate restTemplate(List<HttpMessageConverter<?>> messageConverters) {
-        RestTemplate restTemplate = new RestTemplate();
+    public RestTemplate restTemplate(final List<HttpMessageConverter<?>> messageConverters) {
+        final RestTemplate restTemplate = new RestTemplate();
         restTemplate.setMessageConverters(messageConverters);
         return restTemplate;
     }
 
     @Override
-    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(byteArrayHttpMessageConverter());
+    public void extendMessageConverters(final List<HttpMessageConverter<?>> converters) {
+        converters.add(this.byteArrayHttpMessageConverter());
     }
 
     @Bean
@@ -56,7 +54,7 @@ public class WebConfig implements WebMvcConfigurer {
     public Filter identifyFilter() {
         return new OncePerRequestFilter() {
             @Override
-            protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+            protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response, final FilterChain filterChain) throws ServletException, IOException {
                 response.setHeader("Server", "HangarAuth");
                 filterChain.doFilter(request, response);
             }
@@ -72,8 +70,11 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/avatar/**").allowedOrigins(generalConfig.allowedOrigins());
-        registry.addMapping("/image/**").allowedOrigins(generalConfig.allowedOrigins());
+    public void addCorsMappings(final CorsRegistry registry) {
+        final String[] avatarCorsOrigins = new String[this.generalConfig.allowedOrigins().length + 1];
+        avatarCorsOrigins[0] = this.generalConfig.publicHost();
+        System.arraycopy(this.generalConfig.allowedOrigins(), 0, avatarCorsOrigins, 1, avatarCorsOrigins.length - 1);
+        registry.addMapping("/avatar/**").allowedOrigins(avatarCorsOrigins);
+        registry.addMapping("/image/**").allowedOrigins(this.generalConfig.allowedOrigins());
     }
 }
