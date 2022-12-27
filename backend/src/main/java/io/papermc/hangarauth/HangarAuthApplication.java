a25445cbf3d1359b8836902da051d472415e62bb
MiniDigger | Martin <admin@benndorf.dev>
2022-12-27 23:48:42 +0100

    chore(backend): play around with native images
    doesn't work

--- backend/src/main/java/io/papermc/hangarauth/HangarAuthApplication.java
+++ backend/src/main/java/io/papermc/hangarauth/HangarAuthApplication.java
@@ -1,15 +1,26 @@
 package io.papermc.hangarauth;
 
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan("io.papermc.hangarauth.config.custom")
+@ImportRuntimeHints(HangarAuthApplication.HangarAuthRuntimeHints.class)
 public class HangarAuthApplication {
 
     public static void main(final String[] args) {
         SpringApplication.run(HangarAuthApplication.class, args);
     }
 
+    static class HangarAuthRuntimeHints implements RuntimeHintsRegistrar {
+        @Override
+        public void registerHints(final RuntimeHints hints, final ClassLoader classLoader) {
+            hints.resources().registerResourceBundle("jakarta.servlet.LocalStrings");
+            hints.resources().registerResourceBundle("jakarta.servlet.http.LocalStrings");
+        }
+    }
 }
