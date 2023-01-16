a18dcf1d014a2deb9519f25fb01d2c4e6218a5aa
Nassim Jahnke <nassim@njahnke.dev>
2023-01-16 12:29:11 +0100

    Update HangarLib, update footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -12,6 +12,18 @@ const { t } = useI18n();
           <a href="https://github.com/HangarMC" class="flex items-center rounded-md px-6 py-2" hover="text-primary-400 bg-primary-0">
             {{ t("footer.org") }}
           </a>
+          <a
+            href="https://hangarstatus.papermc.io"
+            class="flex items-center rounded-md px-6 py-2"
+            hover="text-primary-400 bg-primary-0"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            {{ t("hangar.footer.status") }}
+          </a>
+          <a href="https://hangar.papermc.io/terms" class="flex items-center rounded-md px-6 py-2" hover="text-primary-400 bg-primary-0">
+            {{ t("footer.terms") }}
+          </a>
           <a href="https://hangar.papermc.io/privacy" class="flex items-center rounded-md px-6 py-2" hover="text-primary-400 bg-primary-0">
             {{ t("footer.privacypolicy") }}
           </a>
