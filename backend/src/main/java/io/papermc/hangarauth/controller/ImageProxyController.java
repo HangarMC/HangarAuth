a97222f7d76c7d1ccf9e664d5295a89a4bd3af1b
MiniDigger | Martin <admin@benndorf.dev>
2023-03-25 10:28:53 +0100

    feat: validate url in image proxy

--- backend/src/main/java/io/papermc/hangarauth/controller/ImageProxyController.java
+++ backend/src/main/java/io/papermc/hangarauth/controller/ImageProxyController.java
@@ -1,8 +1,11 @@
 package io.papermc.hangarauth.controller;
 
-import io.papermc.hangarauth.config.custom.GeneralConfig;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.UnknownHostException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,17 +17,19 @@ import org.springframework.web.client.RestTemplate;
 public class ImageProxyController {
 
     private final RestTemplate restTemplate;
-    private final GeneralConfig generalConfig;
 
-    public ImageProxyController(final RestTemplate restTemplate, final GeneralConfig generalConfig) {
+    public ImageProxyController(final RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
-        this.generalConfig = generalConfig;
     }
 
     @GetMapping("/**")
     public ResponseEntity<byte[]> proxy(final HttpServletRequest request, final HttpServletResponse response) {
         final String url = this.cleanUrl(request.getRequestURI());
-        return this.restTemplate.getForEntity(url, byte[].class);
+        if (this.validTarget(url)) {
+            return this.restTemplate.getForEntity(url, byte[].class);
+        } else {
+           return ResponseEntity.badRequest().build();
+        }
     }
 
     private String cleanUrl(final String url) {
@@ -32,7 +37,25 @@ public class ImageProxyController {
             .replace("/image/", "")
             .replace("https:/", "https://")
             .replace("http:/", "http://")
-            .replace(this.generalConfig.hangarFrontendHost(), this.generalConfig.hangarBackendHost())
             .replace(":///", "://");
+    }
+
+    private boolean validTarget(final String url) {
+        try {
+            final URL parsedUrl = new URL(url);
+            // valid proto
+            if (!parsedUrl.getProtocol().equals("http") && !parsedUrl.getProtocol().equals("https")) {
+                return false;
+            }
+            final InetAddress inetAddress = InetAddress.getByName(parsedUrl.getHost());
+            // not local ip
+            if (inetAddress.isAnyLocalAddress() || inetAddress.isLoopbackAddress() || inetAddress.isSiteLocalAddress()) {
+                return false;
+            }
+        } catch (final MalformedURLException | UnknownHostException e) {
+            return false;
+        }
+
+        return true;
     }
 }
